ed78342253da7051fa2e0b2043fe017b46a2e0ad
Nisarg Vadgama <nisargvv@gmail.com>
2021-03-30 14:43:08 -0400

    Create Routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-function App() {
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+
+import React from "react";
+import { BookList } from "./components/BookList";
+
+export default function App() {
   return (
-    <h1>Hi</h1>
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/">
+            <BookList />
+          </Route>
+          <Route path="/add-book">
+            <h1>Add Book</h1>
+          </Route>
+          <Route path="/edit-book">
+            <h1>Edit Book</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
-
-export default App;
