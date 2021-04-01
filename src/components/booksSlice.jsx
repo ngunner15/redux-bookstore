0069f6b50cbd603e44a6ee8bd9682d10ed3338c5
Nisarg Vadgama <nisargvv@gmail.com>
2021-04-01 18:36:46 -0400

    Add bookAdded and bookUpdated reducers

--- src/components/booksSlice.jsx
+++ src/components/booksSlice.jsx
@@ -1,14 +1,30 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = [
-  { id: 1, name: 'Inverting The Pyramid', price: '16.99', category: 'sports', description: 'A pioneering soccer book that chronicles the evolution of soccer tactics' },
-  { id: 2, name: 'Batman: Year One', price: '18.09', category: 'graphic novel', description: 'The story recounts Batmans first year as a crime-fighter' }
+  { id: 0, name: 'Inverting The Pyramid', price: '16.99', category: 'sports', description: 'A pioneering soccer book that chronicles the evolution of soccer tactics' },
+  { id: 1, name: 'Batman: Year One', price: '18.09', category: 'graphic novel', description: 'The story recounts Batmans first year as a crime-fighter' }
 ];
 
 const booksSlice = createSlice({
   name: "books",
   initialState,
-  reducers: {},
+  reducers: {
+    bookAdded(state, action) {
+      state.push(action.payload);
+    },
+    bookUpdated(state, action) {
+      const { id, name, price, category, description } = action.payload;
+      const existingBook = state.find((book) => book.id === id);
+      if (existingBook) {
+        existingBook.name = name;
+        existingBook.price = price;
+        existingBook.category = category;
+        existingBook.description = description;
+      }
+    },
+  },
 });
+
+export const { bookAdded, bookUpdated } = booksSlice.actions;
 
 export default booksSlice.reducer;
