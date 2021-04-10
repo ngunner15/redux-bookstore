0f27676ff3bbb60b2cf47dc1d717c4addf5fe006
Nisarg Vadgama <nisargvv@gmail.com>
2021-04-10 14:59:19 -0400

    Add comment

--- src/components/booksSlice.jsx
+++ src/components/booksSlice.jsx
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 const initialState = [
   { id: 0, name: 'Inverting The Pyramid', price: '16.99', category: 'sports', description: 'A pioneering soccer book that chronicles the evolution of soccer tactics' },
   { id: 1, name: 'Batman: Year One', price: '18.09', category: 'graphic novel', description: 'The story recounts Batmans first year as a crime-fighter' },
-];
+];  // default book list
 
 const booksSlice = createSlice({
   name: "books",
